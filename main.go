9cb2cd6e8d8429d71820931b122a78425a4430e5
Matthias Radestock <matthias.radestock@gmail.com>
2016-03-05 08:19:21 +0000

    clean up connections, add replies, add message counter

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	}
 	flag.StringVar(&laddr, "l", "", "listening address")
 	flag.StringVar(&c.msg, "m", hostname, "message to send")
-	flag.DurationVar(&c.timeout, "t", 5*time.Second, "connect/send timeout")
+	flag.DurationVar(&c.timeout, "t", 5*time.Second, "connect/send/recv timeout")
 	flag.DurationVar(&c.retryInterval, "r", 1*time.Second, "connection retry interval")
 	flag.DurationVar(&c.sendInterval, "i", 1*time.Second, "message sending interval")
 	flag.Parse()
@@ -42,7 +42,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go accept(ln)
+		go accept(ln, c)
 	}
 
 	for _, addr := range addrs {
@@ -52,7 +52,7 @@ func main() {
 	select {}
 }
 
-func accept(ln net.Listener) {
+func accept(ln net.Listener, c config) {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -60,23 +60,30 @@ func accept(ln net.Listener) {
 			continue
 		}
 		log.Infof("accepted connection from %s", conn.RemoteAddr())
-		go read(conn)
+		go recv(conn, c)
 	}
 }
 
-func read(conn net.Conn) {
+func recv(conn net.Conn, c config) {
 	r := bufio.NewReader(conn)
 	for {
+		conn.SetDeadline(time.Now().Add(c.timeout))
 		msg, err := r.ReadString('\n')
 		if err != nil {
-			log.Errorf("read from %s failed: %s", conn.RemoteAddr(), err)
+			log.Errorln(err)
 			break
 		}
 		fmt.Print(msg)
+		if _, err := conn.Write([]byte("\n")); err != nil {
+			log.Errorln(err)
+			break
+		}
 	}
+	conn.Close()
 }
 
 func send(addr string, c config) {
+	i := 1
 	for {
 		conn, err := net.DialTimeout("tcp", addr, c.timeout)
 		if err != nil {
@@ -85,13 +92,21 @@ func send(addr string, c config) {
 			continue
 		}
 		log.Infof("connected to %s", conn.RemoteAddr())
+		r := bufio.NewReader(conn)
 		for {
-			conn.SetWriteDeadline(time.Now().Add(c.timeout))
-			if _, err := conn.Write([]byte(c.msg + "\n")); err != nil {
+			conn.SetDeadline(time.Now().Add(c.timeout))
+			if _, err := conn.Write([]byte(fmt.Sprintf("%s %d\n", c.msg, i))); err != nil {
 				log.Errorln(err)
 				break
 			}
+			_, err := r.ReadString('\n')
+			if err != nil {
+				log.Errorln(err)
+				break
+			}
+			i++
 			time.Sleep(c.sendInterval)
 		}
+		conn.Close()
 	}
 }
